8cbeee20906da4be2416d9dd513fef3631ef3ea2
Babis Babalis <babisbabalis@gmail.com>
2021-02-01 17:26:07 +0200

    table updated

--- src/highlight_max_val_per_row.py
+++ src/highlight_max_val_per_row.py
@@ -1,3 +1,9 @@
+""" module to highlight certain cells to dash table.
+
+source: https://dash.plotly.com/datatable/conditional-formatting
+"""
+
+
 import dash
 import dash_table
 import pandas as pd
@@ -22,15 +28,48 @@ def highlight_max_row(df):
         )
     ]
 
+def highlight_above_and_below_max(df):
+    df_numeric_columns = df.select_dtypes('number').drop(['id'], axis=1)
+    mean = df_numeric_columns.mean().mean()
+    return (
+        [
+            {
+                'if': {
+                    'filter_query': '{{{}}} > {}'.format(col, mean),
+                    'column_id': col
+                },
+                'backgroundColor': '#3D9970',
+                'color': 'white'
+            } for col in df_numeric_columns.columns
+        ] +
+        [
+            {
+                'if': {
+                    'filter_query': '{{{}}} <= {}'.format(col, mean),
+                    'column_id': col
+                },
+                'backgroundColor': '#FF4136',
+                'color': 'white'
+            } for col in df_numeric_columns.columns
+        ]
+    )
+
+
 df = pd.read_csv('data/OD2019.csv')
 
 app = dash.Dash(__name__)
 
 df['id'] = df.index
+
 app.layout = dash_table.DataTable(
     data=df.to_dict('records'),
     sort_action='native',
     columns=[{'name': i, 'id': i} for i in df.columns if i != 'id'],
+    style_table={'height':'800px', 'overflowY':'auto'},
+    style_header={
+        'backgroundColor': 'rgb(230, 230, 230)',
+        'fontWeight': 'bold'
+    },
     style_data_conditional=highlight_max_row(df)
 )
 
