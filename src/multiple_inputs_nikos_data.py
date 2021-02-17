353e524d5d338533c18d1542f5e25dd4c5e5333d
Babis Babalis <babisbabalis@gmail.com>
2021-02-17 21:51:40 +0200

    under heavy development. table added. filter for columns added

--- src/multiple_inputs_nikos_data.py
+++ src/multiple_inputs_nikos_data.py
@@ -1,4 +1,5 @@
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
@@ -12,72 +13,41 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = pd.read_csv('data/nikos-data.csv')
-df['Data Since'] = pd.to_numeric(df['Data Since'])
 
-available_indicators = df['Provider'].unique()
-pdb.set_trace()
+## Create application.
+# Table.
+# Filters.
+# Another table from filters.
+
+features = df.columns
 
 app.layout = html.Div([
-    html.H1('Loads Matrix'),
-    html.Br(),
-
     html.Div([
-
-        html.Div([
-            dcc.Dropdown(
-                id='xaxis-column',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Eurostat'
-            ),
-        ],
-        style={'width': '48%', 'display': 'inline-block'}),
-
-        html.Div([
-            dcc.Dropdown(
-                id='yaxis-column',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='ITC'
-            ),
-        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+        html.H1('Dash Application'),
+        dash_table.DataTable(
+            data=df.to_dict('records'),
+            sort_action='native',
+            columns=[{'name': i, 'id': i} for i in df.columns if i != 'id'],
+            style_table={'height':'800px', 'overflowY':'auto'},
+            style_header={
+                'backgroundColor': 'rgb(230, 230, 230)',
+                'fontWeight': 'bold'
+            }),
     ]),
-
-    dcc.Graph(id='indicator-graphic'),
-
-    dcc.Slider(
-        id='year--slider',
-        min=df['Data Since'].min(),
-        max=df['Data Since'].max(),
-        value=df['Data Since'].max(),
-        marks={str(year): str(year) for year in df['Data Since'].unique()},
-        step=None
-    )
+    html.P(),
+    html.Div([
+        html.H2('Filters'),
+        dcc.Dropdown(id='xaxis',
+                     options=[{'label':i, 'value':i} for i in features],
+                     value='displacement')],
+        style={'width':'18%', 'display':'inline-block'}),
 ])
-
-@app.callback(
-    Output('indicator-graphic', 'figure'),
-    Input('xaxis-column', 'value'),
-    Input('yaxis-column', 'value'),
-    Input('year--slider', 'value'))
-def update_graph(xaxis_column_name, yaxis_column_name,
-                 year_value):
-    dff = df[df['Data Since'] == year_value]
-
-    fig = px.scatter(x=dff[dff['Provider'] == xaxis_column_name]['Value'],
-                     y=dff[dff['Provider'] == yaxis_column_name]['Value'],
-                     hover_name=dff[dff['Provider'] == yaxis_column_name]['Time Granularity'])
-
-    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
-
-    #fig.update_xaxes(title=xaxis_column_name)
-
-    #fig.update_yaxes(title=yaxis_column_name)
-
-    return [fig]
-
 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
 
 # import pdb
 # import dash
