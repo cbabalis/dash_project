5684fcda68a6c9afa5946d5f7744ddcca1264594
Babis Babalis <babisbabalis@gmail.com>
2021-03-09 21:30:46 +0200

    modifications according to TODO_09_03.md

--- src/agro_data_1.py
+++ src/agro_data_1.py
@@ -12,12 +12,14 @@ import plotly.express as px
 import pdb
 
 
-sample_df = pd.read_csv('data/agro2018/Agro2018_no_nanBABIS.csv', delimiter='\t')
+sample_df = pd.read_csv('data/03b_Table_Agro18_for_Babis.csv', delimiter='\t')
 sample_df = sample_df.fillna(0)
+PROD_AVAILABILITY = 'Διάθεση Αγροτικών Προϊόντων'
 
-geospatial_categories = ['Κωδικοποίηση NUTS 2', 'Περιφέρεια (NUTS 2)', 'Κωδικοποίηση NUTS 3', 'Περιφερειακή Ενότητα (NUTS 3)']
-product_categories = ['Αγροτικά Προϊόντα', 'Κατηγορία Αγροτικών Προϊόντων', 'Κατηγοριοποίηση Αγροτικών Προϊόντων ανάλογα με την χρήση']
-vals_categories = ['Έκταση (στρέμματα)', 'Ποσότητα Παραγωγής (σε τόνους)', 'Αριθμός Δέντρων	Έτος Αναφοράς']
+geospatial_names = ['Επιλογή με βάση το όνομα της Περιφέρειας (NUTS2)','Επιλογή με βάση τον κωδικό NUTS2 της περιφέρειας', 'Επιλογή με βάση το όνομα του Νομού (NUTS3)','Επιλογή με βάση τον κωδικό NUTS3 του Νομού']
+geospatial_categories = ['κωδ. NUTS2', 'Περιφέρειες (NUTS2)', 'κωδ. NUTS3', 'Περ. Ενότητες (NUTS3)']
+product_categories = ['Αγροτικά Προϊόντα', 'Κατηγορίες Αγροτικών Προϊόντων']
+vals_categories = ['Εκτάσεις (σε στρέμματα)', 'Παραγωγή (σε τόνους)', 'Πλήθος Δέντρων	Έτος Αναφοράς']
 chart_types = ['Γράφημα Στήλης', 'Γράφημα Πίτας']
 
 
@@ -35,6 +37,8 @@ def get_bar_figure(dff, x_col, y_col, col_sum):
     fig = px.bar(dff, x=x_col, y=y_col, color=col_sum) #x_col)
 
     fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+    
+    fig.update_traces( textposition='auto')
 
     fig.update_xaxes(title=y_col)
 
@@ -45,6 +49,7 @@ def get_bar_figure(dff, x_col, y_col, col_sum):
 
 def get_pie_figure(dff, x_col, col_sum, y_col):
     fig = px.pie(dff, values=col_sum, names=y_col)
+    fig.update_traces(textposition='inside', textinfo='percent', hoverinfo='label+value', textfont_size=20)
     return fig
 
 
@@ -62,16 +67,17 @@ app.layout = html.Div([
     html.Div([
         html.Label("Επιλέξτε Εδαφικές Μονάδες"),
         dcc.Dropdown(id='countries-radio',
-                          options=[{'label': k, 'value': k} for k in geospatial_categories],
+                          options=[{'label': l, 'value': k} for l, k in zip(geospatial_names, geospatial_categories)],
                           value=''),
         html.Label("Επιλέξτε Περιοχές"),
         dcc.Dropdown(id='cities-radio', multi=True),],
-                                style = {'width': '350px',
+                                style = {'width': '400px',
                                     'fontSize' : '15px',
-                                    'padding-left' : '100px',
+                                    'padding-left' : '50px',
                                     'display': 'inline-block'}),
     # product filters
-    html.Div([html.Label("Επιλέξτε Κατηγορίες ή αγροτικά προϊόντα"),
+    html.Div([
+        html.Label("Επιλέξτε Αγροτικά Προϊόντα"),
         dcc.Dropdown(id='products-radio',
                           options=[{'label': k, 'value': k} for k in product_categories],
                           value=''),
@@ -79,17 +85,20 @@ app.layout = html.Div([
         dcc.Dropdown(id='products-radio-val', multi=True),],
                                 style = {'width': '350px',
                                     'fontSize' : '15px',
-                                    'padding-left' : '100px',
+                                    'padding-left' : '50px',
                                     'display': 'inline-block'}),
     
     # values filters
-    html.Div([html.Label("Επιλέξτε Τιμή Προβολής"),
+    html.Div([
+        html.Label("Επιλέξτε τύπο διάθεσης Αγροτικών Προϊόντων"),
+        dcc.Dropdown(id='availability-radio'),
+        html.Label("Επιλέξτε Τιμή Προβολής"),
         dcc.Dropdown(id='column-sum',
                           options=[{'label': k, 'value': k} for k in vals_categories],
                           value=''),
     ],style = {'width': '350px',
                                     'fontSize' : '15px',
-                                    'padding-left' : '100px',
+                                    'padding-left' : '50px',
                                     'display': 'inline-block'}),
     html.Div([html.Label("Επιλογή Γραφήματος"),
         dcc.Dropdown(id='chart-choice',
@@ -97,7 +106,7 @@ app.layout = html.Div([
                           value='Γράφημα Στήλης'),
     ],style = {'width': '350px',
                                     'fontSize' : '15px',
-                                    'padding-left' : '100px',
+                                    'padding-left' : '50px',
                                     'display': 'inline-block'}),
     # table here
     html.Hr(),
@@ -137,14 +146,25 @@ def set_products_value(available_options):
 
 
 @app.callback(
+    Output('availability-radio', 'options'),
+    Input('availability-radio', 'value'))
+def set_products_options(selected_country):
+    return [{'label': i, 'value': i} for i in sample_df[PROD_AVAILABILITY].unique()]
+
+
+
+@app.callback(
     Output('display-selected-table', 'children'),
     [Input('countries-radio', 'value'),
     Input('cities-radio', 'value'),
     Input('products-radio', 'value'),
     Input('products-radio-val', 'value'),
+    Input('availability-radio', 'value')
     ])
-def set_display_table(selected_country, selected_city, selected_prod_cat, selected_prod_val):
+def set_display_table(selected_country, selected_city, selected_prod_cat, selected_prod_val, availability_prod_val):
     dff = get_col_rows_data(selected_country, selected_city, sample_df)
+    if (availability_prod_val):
+        dff = dff[dff[PROD_AVAILABILITY] == availability_prod_val]
     df_temp = get_col_rows_data(selected_prod_cat, selected_prod_val, dff)
     return html.Div([
         dash_table.DataTable(
@@ -169,10 +189,18 @@ def set_display_table(selected_country, selected_city, selected_prod_cat, select
                 'width': '100%',
                 'minWidth': '10%',
             },
-            style_header={'backgroundColor': 'rgb(200,200,200)'},
-            style_cell={'backgroundColor': 'rgb(230,230,230)','color': 'black','height': 'auto','width': 'auto'},#minWidth': '0px', 'maxWidth': '180px', 'whiteSpace': 'normal'},
+            style_header={'backgroundColor': 'rgb(200,200,200)', 'width':'auto'},
+            style_cell={'backgroundColor': 'rgb(230,230,230)','color': 'black','height': 'auto','minWidth': '100px', 'width': '150px', 'maxWidth': '180px','overflow': 'hidden', 'textOverflow': 'ellipsis', },#minWidth': '0px', 'maxWidth': '180px', 'whiteSpace': 'normal'},
             #style_cell={'minWidth': '120px', 'width': '150px', 'maxWidth': '180px'},
-              style_data={'whiteSpace': 'auto','height': 'auto','width': 'auto'}
+            style_data={'whiteSpace': 'auto','height': 'auto','width': 'auto'},
+            tooltip_data=[
+            {
+                column: {'value': str(value), 'type': 'markdown'}
+                for column, value in row.items()
+            } for row in df_temp.to_dict('records')
+            ],
+            tooltip_header={i: i for i in df_temp.columns},
+    tooltip_duration=None
         )
     ])
 
@@ -186,7 +214,7 @@ def set_sum_values(available_options):
 
 @app.callback(
     Output('chart_choice', 'value'),
-    Input('hart_choice', 'options'))
+    Input('chart_choice', 'options'))
 def get_chart_choice(available_options):
     return available_options[0]['value']
 
@@ -211,8 +239,6 @@ def set_display_figure(x_col, x_col_vals, y_col, y_col_vals, col_sum, chart_type
         fig = get_pie_figure(dff, x_col, col_sum, y_col)
 
     return fig
-
-
 
 
 
